dd629df3a3340bb118cdb530bc36328ba5c7ea42
kainklob <klobk23@gmail.com>
2022-08-23 13:54:58 -0400

    finished separate-positive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+// input: array of non-zero ints.
+// returns: all positive ints on left, all negative ints or right. Non-sorted. Must be same array passed in. Not a copy
+function separatePositive(intArray) {
+    let left = 0;
+    let right = intArray.length - 1;
+
+    while (left <= right) {
+        if (intArray[left] < 0) {
+            let val = intArray.splice(left, 1);
+            intArray.push(val);
+        } else left++;
+
+        if (intArray[right] > 0) {
+            let val = intArray.splice(right, 1);
+            intArray.unshift(val);
+        } else right--;
+    }
+
+    return intArray;
+
 }
+module.exports = { separatePositive };
