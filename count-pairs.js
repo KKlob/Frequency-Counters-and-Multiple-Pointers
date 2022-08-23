6b6c4f826d39b0530039bd7d8021764187c54f34
kainklob <klobk23@gmail.com>
2022-08-23 14:32:11 -0400

    finished count-pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+// inputs: array of ints, a target sum
+// find number of pairs of ints in array whose sum is equal to target sum
+// assume no duplicate values in array
+// returns: num of pairs that add to target
+function countPairs(intArray, target) {
+    let count = 0;
+    intArray.forEach(int => {
+        let dif = target - int;
+        if (intArray.includes(dif) && dif !== int) {
+            count++;
+        }
+    });
+
+    return count / 2;
+
 }
+
+module.exports = { countPairs }
